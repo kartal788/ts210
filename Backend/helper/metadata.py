18c91e8c05458f128025fc8e7fd7b0cc2869d4a5
kartal788 <146070323+kartal788@users.noreply.github.com>
2026-03-16 01:04:56 +0300

    Update metadata.py

--- Backend/helper/metadata.py
+++ Backend/helper/metadata.py
@@ -147,6 +147,7 @@ async def _tmdb_movie_details(movie_id):
     try:
         async with API_SEMAPHORE:
             details = await tmdb.movie(movie_id).details(
+                language="tr-TR",
                 append_to_response="external_ids,credits"
             )
             images = await tmdb.movie(movie_id).images()
@@ -166,6 +167,7 @@ async def _tmdb_tv_details(tv_id):
     try:
         async with API_SEMAPHORE:
             details = await tmdb.tv(tv_id).details(
+                language="tr-TR",
                 append_to_response="external_ids,credits"
             )
             images = await tmdb.tv(tv_id).images()
@@ -184,7 +186,7 @@ async def _tmdb_episode_details(tv_id, season, episode):
         return EPISODE_CACHE[key]
     try:
         async with API_SEMAPHORE:
-            details = await tmdb.episode(tv_id, season, episode).details()
+            details = await tmdb.episode(tv_id, season, episode).details(language="tr-TR")
         EPISODE_CACHE[key] = details
         return details
     except Exception:
